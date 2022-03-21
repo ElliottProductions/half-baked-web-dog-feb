17b58ce670e34f4d902f47b879b18b75e15e4b7d
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-21 11:16:00 -0800

    put in the basic code for the fetch functions

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,18 +1,26 @@
 const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
 
-const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const client = client.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function getDogs() {
     // from the dogs table, select all items
-
+    const response = await client
+        .from('dogs')
+        .select('*');
     // and return the response (checking for errors)
-    return checkError(response);    
+    //return checkError(response);    
+    return response.body;
 }
 
 export async function getDog(id) {
     // from the dogs table, select a single dog who has the matching id
+    const response = await client
+        .from('ice_creams')
+        .select('*')
+        .match({ id: 4 })
 
+        .single(); //keeps from returning an array with one item
     // and return the response (checking for errors)
     return checkError(response);    
 }
